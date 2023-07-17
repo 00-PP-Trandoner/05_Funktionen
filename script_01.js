55530818917984e1abf867e030f6de661da1f929
Normann <trand_oner@gmx.de>
2023-07-17 11:01:12 +0200

    Parametrisierung + Datenübergabe von AUSSEN

--- script_01.js
+++ script_01.js
@@ -6,28 +6,43 @@
 
 // Funktionsrumpf (Body)  callee
 // Funktionsdeklaration 
-function test ()
+// function test ()
 {
-    console.log("Hallo Tobias");
+    // console.log("Hallo Tobias");
 }
 
 // Funktionsaufruf (call)
-test();
+// test();
 
 
 /***** Funktionen 02a *****/
 
 // 2a. Parametrisierung + Datenübergabe von INNEN
 
-ausgabeNamen();
+// ausgabeNamen();
 
-function ausgabeNamen() 
-{ //<---Kennzeichnet einen CodeBlock!"ANFANG"
+function ausgabeNamen(){} 
+// { //<---Kennzeichnet einen CodeBlock!"ANFANG"
     // console.log("Hallo Tobias!")
     // console.log("Hallo Cagri!");
-    let firstName = "Yasmeen";
-    console.log("Hallo " + firstName + "!");
-}//<-- CodeBlock! "ENDE"
+    // let firstName = "Yasmeen";
+    // console.log("Hallo " + firstName + "!");
+// }//<-- CodeBlock! "ENDE"
 
 // console.log(firstName); //Fehler --> SCOPE! 
 
+
+/***** Funktionen 02b *****/
+
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+
+// Argumente sind Daten für Parameter !!!
+ausgabeName2("Normann"); // call + Argument(e)
+ausgabeName2("Max");
+ausgabeName2("Michelle")
+
+// Funktion mit Parametern !!!
+function ausgabeName2(firstName) {
+    console.log("Hallo " + firstName + "!");
+}
