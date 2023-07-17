9f613c63f1ae0310ed97e0ccb04c3cd3cc7b1219
Normann <trand_oner@gmx.de>
2023-07-17 10:46:11 +0200

    2a. Parametrisierung +

--- script_01.js
+++ script_01.js
@@ -3,3 +3,31 @@
 /***** Funktionen 01 *****/
 // 1. Kapselung von Codeblöcken
 
+
+// Funktionsrumpf (Body)  callee
+// Funktionsdeklaration 
+function test ()
+{
+    console.log("Hallo Tobias");
+}
+
+// Funktionsaufruf (call)
+test();
+
+
+/***** Funktionen 02a *****/
+
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+
+function ausgabeNamen() 
+{ //<---Kennzeichnet einen CodeBlock!"ANFANG"
+    // console.log("Hallo Tobias!")
+    // console.log("Hallo Cagri!");
+    let firstName = "Yasmeen";
+    console.log("Hallo " + firstName + "!");
+}//<-- CodeBlock! "ENDE"
+
+// console.log(firstName); //Fehler --> SCOPE! 
+
